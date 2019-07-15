70c9106daace8ed9ddf0ae8d894a6adbc5992fd8
CurtLH <CurtLHampton@gmail.com>
2019-07-14 21:25:48 -0700

    Collect all ads and not just macbook pro ads

--- get_ads.py
+++ get_ads.py
@@ -32,7 +32,7 @@ except:
     logging.info("Unable to connect to the database")
 
 # create the table to store the results
-cur.execute("""CREATE TABLE IF NOT EXISTS mbp_raw
+cur.execute("""CREATE TABLE IF NOT EXISTS apple_refurb_raw
                (id SERIAL,
                 datetime timestamp default current_timestamp,
                 url varchar,
@@ -45,8 +45,7 @@ soup = bs(r.content, "html.parser")
 ads = soup.find("div",{"class":"refurbished-category-grid-no-js"})
 for a in ads.find_all('a', href=True):
     url = "https://www.apple.com" + a['href'].split('?')[0]
-    if 'macbook-pro' in url.lower():
-        urls.append(url)
+    urls.append(url)
 logging.info("URLs obtained: {}".format(len(urls)))
 
 # collect content on the webpage for each URL
@@ -55,9 +54,8 @@ cnt = 0
 for url in urls:
     r = requests.get(url)
     if r.status_code == 200:
-
-        cur.execute("""INSERT INTO mbp_raw (html)
-                       VALUES (%s)""", [r.text])
+        cur.execute("""INSERT INTO apple_refurb_raw (url, html)
+                       VALUES (%s)""", [url, r.text])
         logging.info("New record inserted into database")
         cnt += 1
     
